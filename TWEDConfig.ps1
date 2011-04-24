25c1cc843882356353fdc556e7f8a14eb349e71d
unclephil <koenigphil@gmail.com>
2011-04-24 20:51:32 +0200

    First commit

---
+++ TWEDConfig.ps1
@@ -0,0 +1,27 @@
+﻿## TiddlyEsxDoc Config file
+####################################
+# VMware VirtualCenter server name #
+####################################
+$vcserver="VCS.ESX.CIRB.LAN" 
+$portvc="443"
+$uc="report"
+$pw="Report*+"
+
+####################################
+##Running variables
+###################
+$vmlimit = 0  ## ZERO equal No limit of analyzed Vm
+
+##################
+# Mail variables #
+##################
+$enablemail = "no"
+$smtpServer = "relay.irisnet.be"
+$mailfrom = "vmadmin <vmadmin@cirb.irisnet.be>"
+$mailto = "pkoenig@cirb.irisnet.be"
+
+#################
+# Script parameters
+###################
+$curdir="c:\ps\autotask\" # the script base path with traling backslash
+$basetemplate = "CMDB_Mini.html"  # template name relative to script path w
